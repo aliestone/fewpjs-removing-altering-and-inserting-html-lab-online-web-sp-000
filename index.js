ac724e50f735b35fce5dae9e6c5e561275cf5468
Alie Stone <helloaliestone@gmail.com>
2021-01-14 04:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code here!
 
-main.remove();
-var newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.innerHTML = "Your name is the champion!";
-document.body.appendChild(newHeader);
+function run (){
+  main.remove();
+  var newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "Your name is the champion!";
+  document.body.appendChild(newHeader);
+}
+
+run()
