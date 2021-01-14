f744fccd64037241f01e8f66ad0d09fee23e8dae
Alie Stone <helloaliestone@gmail.com>
2021-01-14 04:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code here!
 
 main.remove()
-document.body.appendChild('newHeader')
-let newHeader = document.createElement('h1#victory')
+let newHeader = document.body.appendChild(h1#victory);
